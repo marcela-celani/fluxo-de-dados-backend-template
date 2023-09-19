d49bdf02d7071f60d97021d12705b4420680fb96
marcela-celani <marcelacelani.ti@gmail.com>
2023-09-18 21:00:11 -0300

    exercicio resolvido

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express'
 import cors from 'cors'
 import { accounts } from './database'
-import { ACCOUNT_TYPE } from './types'
+import { ACCOUNT_TYPE, TAccount } from './types'
 
 const app = express()
 
@@ -20,35 +20,82 @@ app.get("/accounts", (req: Request, res: Response) => {
     res.send(accounts)
 })
 
-app.get("/accounts/:id", (req: Request, res: Response) => {
-    const id = req.params.id
+app.get("/accounts/:id", (req: Request, res: Response): void => {
+    try {
+        const id: string = req.params.id
 
-    const result = accounts.find((account) => account.id === id) 
+        const result: TAccount | undefined = accounts.find((account) => account.id === id) 
 
-    res.status(200).send(result)
-})
+        if (!result) {
+            res.statusCode = 404
+            throw new Error("Conta não encontrada. Verifique o 'id'.")
+        } else {
+            res.status(200).send(result)
+        }
 
-app.delete("/accounts/:id", (req: Request, res: Response) => {
-    const id = req.params.id
-
-    const accountIndex = accounts.findIndex((account) => account.id === id)
-
-    if (accountIndex >= 0) {
-        accounts.splice(accountIndex, 1)
+    } catch (error) {
+        res.send(error.message)
     }
-
-    res.status(200).send("Item deletado com sucesso")
+    
 })
 
-app.put("/accounts/:id", (req: Request, res: Response) => {
-    const id = req.params.id
+app.delete("/accounts/:id", (req: Request, res: Response): void => {
+    try {
+        const id: string = req.params.id
 
+        //ou, id[0]!=='a'
+        if (!id.startsWith('a')){
+            res.statusCode = 400
+            throw new Error("'id' inválido. Deve iniciar com a letra 'a'")
+        }
+
+        const accountIndex: number = accounts.findIndex((account) => account.id === id)
+
+        if (accountIndex >= 0) {
+            accounts.splice(accountIndex, 1)
+        }
+
+        res.status(200).send("Item deletado com sucesso")
+
+    } catch (error) {
+        console.log(error)
+       if(error instanceof Error){
+            res.send(error.message)
+        } else {
+            res.send('Erro inesperado')
+        }
+    }
+})
+
+app.put("/accounts/:id", (req: Request, res: Response): void => {
+    try {
+    const id: string = req.params.id
     const newId = req.body.id as string | undefined
     const newOwnerName = req.body.ownerName as string | undefined
     const newBalance = req.body.balance as number | undefined
     const newType = req.body.type as ACCOUNT_TYPE | undefined
 
-    const account = accounts.find((account) => account.id === id) 
+    if(!newId.startsWith('a')){
+        res.statusCode = 400
+        throw new Error("'id' deve iniciar com a letra 'a'")
+    }
+
+    if(typeof newOwnerName !== 'string' || newOwnerName.length < 2){
+        res.statusCode = 400
+        throw new Error("'ownerName' deve possuir no mínimo 2 caracteres")
+    }
+
+    if(typeof newBalance !== 'number' || newBalance < 0){
+        res.statusCode = 404
+        throw new Error("newBalance deve ser do tipo 'number'")
+    }
+
+    if(newType !== ACCOUNT_TYPE.BLACK && newType !== ACCOUNT_TYPE.PLATINUM && newType !== ACCOUNT_TYPE.GOLD){
+        res.statusCode = 404
+        throw new Error("newType inválido, deve ser do tipo 'black', 'gold' ou 'platinum'")
+    }
+
+    const account: TAccount | undefined = accounts.find((account) => account.id === id) 
 
     if (account) {
         account.id = newId || account.id
@@ -59,4 +106,10 @@ app.put("/accounts/:id", (req: Request, res: Response) => {
     }
 
     res.status(200).send("Atualização realizada com sucesso")
+    } catch (error) {
+        console.log(error)
+        if(error instanceof Error){
+            res.send(error.message)
+        }
+    }
 })
